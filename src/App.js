de18739390bf5287dc631ff936178e6980345d96
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-02-23 21:29:04 +0100

    add materialize-css and material icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   return (
     <div className="App">
+      <p>Test</p>
     </div>
   );
 }
