a870ce0e8ce22acf1b4e320ee3b1698d5889365c
Jeroen Roumen <jeroen.roumen@deltatre.com>
2020-02-23 21:30:52 +0100

    convert App component to an arrow function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
 
-function App() {
+const App = () => {
 
   useEffect(() => {
     M.AutoInit();
@@ -15,6 +15,6 @@ function App() {
       <p>Test</p>
     </div>
   );
-}
+};
 
 export default App;
