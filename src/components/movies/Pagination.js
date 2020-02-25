c0d80459fafa374320962fecac32b9144facf107
Jeroen Roumen <jeroen.roumen@massive.co>
2020-02-25 15:50:39 +0100

    implement pagination clicks

--- src/components/movies/Pagination.js
+++ src/components/movies/Pagination.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Pagination = ({ loading, movie: { movies, page, total_pages } }) => {
+const Pagination = ({ loading, movie: { movies, page, total_pages }, click }) => {
 
   if (!loading && movies.length !== 0) {
     return (
       <ul className="pagination center">
-        <li className={page === 1 ? 'disabled' : 'waves-effect'}><a href="#!"><i className="material-icons">chevron_left</i></a></li>
+        <li className={page === 1 ? 'disabled' : 'waves-effect'}>
+          <a href="#!" onClick={() => click(page-1)}><i className="material-icons">chevron_left</i></a>
+        </li>
         <span>{(movies.length * page - movies.length) + 1} - {movies.length * page} of {total_pages * movies.length}</span>
-        <li className={page === total_pages ? 'disabled' : 'waves-effect'}><a href="#!"><i className="material-icons">chevron_right</i></a></li>
+        <li className={page === total_pages ? 'disabled' : 'waves-effect'}>
+          <a href="#!" onClick={() => click(page+1)}><i className="material-icons">chevron_right</i></a>
+        </li>
       </ul>
     )
   } else {
@@ -20,4 +24,4 @@ const mapStateToProps = state => ({
   movie: state.movie
 });
 
-export default connect(mapStateToProps, null)(Pagination);
+export default connect(mapStateToProps)(Pagination);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
+import { connect } from 'react-redux';
 import Movies from '../components/movies/Movies';
-import Search from '../components/movies/Search';
 import Pagination from '../components/movies/Pagination';
+import { searchMovies } from '../actions/movieActions';
+import styled from 'styled-components';
 
-const Home = () => {
+const Home = ({ searchMovies }) => {
+
+  const text = useRef('');
+
+  const onChange = (page = 1) => {
+    searchMovies(text.current.value, page);
+  };
+
+  const handlePagination = pageNumber => {
+    onChange(pageNumber);
+  };
+
   return (
     <Fragment>
       <div className="container">
-        <Search />
+        <div>
+          <form>
+            <input
+              id="search"
+              type="search"
+              placeholder="Search movies..."
+              ref={text}
+              onChange={onChange} />
+          </form>
+          <Button className="blue">Clear</Button>
+        </div>
         <Movies />
-        <Pagination />
+        <Pagination click={handlePagination} />
       </div>
     </Fragment>
   )
 };
 
-export default Home;
+const Button = styled.button`
+    display: inline-block;
+    color: #FFFFFF;
+    padding: 0.4rem 2.3rem;
+    font-size: 1rem;
+    border: none;
+    cursor: pointer;
+    margin-right: 0.5rem;
+    transition: opacity 0.2s ease-in;
+    outline: none;
+    display: block;
+    width: 100%;
+`;
+
+export default connect(null, { searchMovies })(Home);
