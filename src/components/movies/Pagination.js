ad93edd103e822775ee980f5a47f8d0e32d85fd2
Jeroen Roumen <jeroen.roumen@massive.co>
2020-02-25 14:19:32 +0100

    remove unused code

--- src/components/movies/Pagination.js
+++ src/components/movies/Pagination.js
@@ -6,33 +6,14 @@ const Pagination = ({ loading, movie: { movies, page, total_pages } }) => {
   if (!loading && movies.length !== 0) {
     return (
       <ul className="pagination center">
-        <li className="disabled"><a href="#!"><i className="material-icons">chevron_left</i></a></li>
+        <li className={page === 1 ? 'disabled' : 'waves-effect'}><a href="#!"><i className="material-icons">chevron_left</i></a></li>
         <span>{(movies.length * page - movies.length) + 1} - {movies.length * page} of {total_pages * movies.length}</span>
-        <li className="waves-effect"><a href="#!"><i className="material-icons">chevron_right</i></a></li>
+        <li className={page === total_pages ? 'disabled' : 'waves-effect'}><a href="#!"><i className="material-icons">chevron_right</i></a></li>
       </ul>
     )
   } else {
-    return <span>{' '}</span>
+    return null;
   }
-
-  // let pageNumbers = [];
-  //
-  // if (!loading && total_pages !== null) {
-  //   for (let i = 1; i <= total_pages; i++) {
-  //     pageNumbers.push(i);
-  //   }
-  // }
-
-  // return (
-  //   <ul className="pagination">
-  //     {pageNumbers.map(number => (
-  //       <li key={number}
-  //           className={ page === number ? 'active blue' : 'waves-effect' }>
-  //         <a href="#!">{number}</a>
-  //       </li>
-  //     ))}
-  //   </ul>
-  // )
 };
 
 const mapStateToProps = state => ({
